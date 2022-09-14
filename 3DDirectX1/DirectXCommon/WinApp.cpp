96900a4dbbcf38e99151c07b5e605cb226346a15
125moti <k020g1385@g.neec.ac.jp>
2022-09-14 17:21:51 +0900

    タイトル変えた

--- 3DDirectX1/DirectXCommon/WinApp.cpp
+++ 3DDirectX1/DirectXCommon/WinApp.cpp
@@ -1,6 +1,6 @@
 #include "WinApp.h"
 #include"../resource.h"
-const wchar_t WinApp::windowClassName[] = L"DirectXGame";
+const wchar_t WinApp::windowClassName[] = L"3029_Land_of_wizards";
 
 LRESULT WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 	// メッセージで分岐
